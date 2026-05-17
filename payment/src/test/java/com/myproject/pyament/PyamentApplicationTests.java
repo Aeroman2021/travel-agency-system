f1fe6d578cf8cb6e36edfa95609b9d0600f9cb5b
Mohsen Malakouti <malakouti.aero@gmail.com>
2026-05-17 16:24:21 +0330

    first commit

---
+++ payment/src/test/java/com/myproject/pyament/PyamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.pyament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
