695710416dc0499e9c8ff880ab7d96411d8a29ab
Mohsen Malakouti <malakouti.aero@gmail.com>
2026-05-20 23:47:06 +0330

    feature: BookingFacade added.

---
+++ booking/src/main/java/com/myproject/application/BookingFacade.java
@@ -0,0 +1,5 @@
+package com.myproject.application;
+
+public interface BookingFacade {
+    int getPassengersCountById(Long bookingId);
+}
---
+++ booking/src/main/java/com/myproject/application/impl/BookingFacadeImpl.java
@@ -0,0 +1,22 @@
+package com.myproject.application.impl;
+
+import com.myproject.application.BookingFacade;
+import com.myproject.exception.ResourceNotFoundException;
+import com.myproject.repository.BookingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookingFacadeImpl implements BookingFacade {
+
+    private final BookingRepository bookingRepository;
+
+    @Override
+    public int getPassengersCountById(Long bookingId) {
+        var booking =  bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new ResourceNotFoundException("%s with id %d not found"
+                        .formatted("Booking", bookingId)));
+        return booking.getNumberOfPassengers();
+    }
+}
