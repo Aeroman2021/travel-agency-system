6f372c95705bfd828e02bbbbfcdfc954d9f977d6
Mohsen Malakouti <malakouti.aero@gmail.com>
2026-05-21 12:24:36 +0330

    feature: An API for  saving the passengers modified.

---
+++ passanger/src/main/java/com/myproject/event/PassengerRegisteredEvent.java
@@ -0,0 +1,16 @@
+package com.myproject.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PassengerRegisteredEvent {
+    Long bookingId;
+}
