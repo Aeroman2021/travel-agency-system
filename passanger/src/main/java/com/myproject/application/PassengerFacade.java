f8393c2b5f0aa16598c6817b77c2318d32f72d22
Mohsen Malakouti <malakouti.aero@gmail.com>
2026-05-21 22:36:18 +0330

    Feature: A method for retrieving passengers by booking id added to the Passenger Facade.

---
+++ passanger/src/main/java/com/myproject/application/PassengerFacade.java
@@ -0,0 +1,9 @@
+package com.myproject.application;
+
+import com.myproject.model.dto.PassengerResponseDto;
+
+import java.util.List;
+
+public interface PassengerFacade {
+    List<PassengerResponseDto> getPassengerByBookId(Long bookId);
+}
