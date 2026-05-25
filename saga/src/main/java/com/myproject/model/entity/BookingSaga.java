69ff43aefdb16b0fffe7452cb0917d517fa34daf
Mohsen Malakouti <malakouti.aero@gmail.com>
2026-05-25 21:34:39 +0330

    feature: Saga module created and BookingSaga entity added.

---
+++ saga/src/main/java/com/myproject/model/entity/BookingSaga.java
@@ -0,0 +1,35 @@
+package com.myproject.model.entity;
+
+import com.myproject.model.enums.SagaStatus;
+import com.myproject.model.enums.SagaStep;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class BookingSaga {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long bookingId;
+
+    @Enumerated(EnumType.STRING)
+    private SagaStep currentStep;
+
+    @Enumerated(EnumType.STRING)
+    private SagaStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+}
---
+++ saga/src/main/java/com/myproject/model/enums/SagaStatus.java
@@ -0,0 +1,11 @@
+package com.myproject.model.enums;
+
+public enum SagaStatus {
+    STARTED,
+    FLIGHT_RESERVED,
+    PASSENGERS_REGISTERED,
+    PAYMENT_COMPLETED,
+    TICKET_ISSUED,
+    FAILED,
+    COMPENSATED
+}
