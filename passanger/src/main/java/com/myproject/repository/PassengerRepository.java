23ddf3b2e771ad4780cb311691ad3e04a2e74024
Mohsen Malakouti <malakouti.aero@gmail.com>
2026-05-26 23:09:11 +0330

    feature: All scripts for generating saga table and updating passenger table added.Moreover Passenger entity updated.

--- passanger/src/main/java/com/myproject/repository/PassengerRepository.java
+++ passanger/src/main/java/com/myproject/repository/PassengerRepository.java
@@ -7,8 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PassengerRepository extends JpaRepository<Passenger,Long> {
-    List<PassengerResponseDto> getPassengersByBookingId(Long bookingId);
+    List<Passenger> getPassengersByBookingId(Long bookingId);
     void deletePassengerByBookingId(Long bookingId);
     List<Passenger> findPassengersByBookingId(Long bookingId);
-    List<Passenger> getPassengersByBookingId(Long bookingId);
 }
